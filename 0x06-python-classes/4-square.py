8b7d060d7e524b88ce33d2f969c0bcf9916ce299
KidCoder-Eth <cfc.kidmat@gmail.com>
2022-09-13 22:36:12 +0300

    corr

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,58 +1,34 @@
 #!/usr/bin/python3
-"""Module Square"""
+"""Module Square."""
 
 
 class Square:
-    """ Square class defined by geometric shap
-
-        Attributes:
-            size (int): Size of square
+    """Represents a square.
+    Private instance attribute: size:
+        - property def size(self)
+        - property setter def size(self, value)
+    Instantiation with optional size.
+    Public instance method: def area(self).
     """
+
     def __init__(self, size=0):
-        """initializes the square
-        Args:
-            size (int): size of a side of the square
-
-        Returns:
-            None
-        """
+        """Initializes the data."""
         self.__size = size
-
-    def area(self):
-        """
-        set square square area
-
-        Return:
-            the current square area (int)
-        """
-        return self.__size ** 2
 
     @property
     def size(self):
-        """
-        getter of size
-
-        Return:
-            Size of square
-        """
+        """Retrieves the size."""
         return self.__size
 
     @size.setter
     def size(self, value):
-        """
-        Setter of size
-
-        Args:
-            size (int): size of a side of the square
-        Raises
-            TypeError: if size is not int
-            ValueError: size less than 0
-        Returns:
-            None
-        """
-        if type(value) is not int:
+        """Sets the size to a value."""
+        if not isinstance(value, int):
             raise TypeError("size must be an integer")
         elif value < 0:
-                raise ValueError("size must be >= 0")
-        else:
-            self.__size = value
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Returns the current square area."""
+        return self.__size ** 2
