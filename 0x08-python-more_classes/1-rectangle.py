e1d8070ac34428eee4ffc12f31883b54308de9af
KidCoder-Eth <cfc.kidmat@gmail.com>
2022-09-19 14:24:16 -0700

    correction

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -6,7 +6,7 @@ a Rectangle class.
 
 class Rectangle:
     """Rectangle class defined by width and height."""
-    
+
     def __init__(self, width=0, height=0):
         """Initializes a Rectangle instance.
         Args:
