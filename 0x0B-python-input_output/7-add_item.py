04c21028cfb96731de673ad47601d912d78df23b
KidCoder-Eth <cfc.kidmat@gmail.com>
2022-09-28 16:27:16 -0700

    Task-7 correction

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -8,8 +8,8 @@ import sys
 import json
 import os.path
 
-save_to_json_file = __import__("7-save_to_json_file").save_to_json_file
-load_from_json_file = __import__("8-load_from_json_file").load_from_json_file
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
 my_file = 'add_item.json'
 
@@ -23,4 +23,3 @@ if len(sys.argv) > 1:
         my_list.append(elem)
 
 save_to_json_file(my_list, my_file)
-
