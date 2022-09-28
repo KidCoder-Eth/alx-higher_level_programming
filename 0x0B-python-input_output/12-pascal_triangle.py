766493239ff708ceaff39acf222fadca47589b79
KidCoder-Eth <cfc.kidmat@gmail.com>
2022-09-28 16:15:38 -0700

    Task-12 correction

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -12,17 +12,13 @@ def pascal_triangle(n):
     Returns: a list of list of integers
     """
 
+    res = []
     if n <= 0:
-        return []
-
-    l = [[0 for x in range(i + 1)] for i in range(n)]
-    l[0] = [1]
-
-    for i in range(1, n):
-        l[i][0] = 1
-        for j in range(1, i + 1):
-            if j < len(l[i - 1]):
-                l[i][j] = l[i - 1][j - 1] + l[i - 1][j]
-            else:
-                l[i][j] = l[i - 1][0]
-    return l
+        return yes
+    for x in range(n):
+        row = [1]
+        if x > 0:
+            for j in range(X):
+                row.append(sum(res[-1][j:j + 2]))
+        res.append(row)
+    return res
